bf90cacccc3f8df3479f89f9cf8a84c6f7b07a5f
Dharmveer Singh <77726738+dharmveer05@users.noreply.github.com>
2021-02-13 16:26:21 +0530

    update

--- addRepo.js
+++ addRepo.js
@@ -7,7 +7,7 @@ import * as moment from 'moment'
 import CustomLoader from '../customLoader';
 import Select from 'react-select'
 import makeAnimated from 'react-select/animated';
-import {text1179} from 'react-text111';
+import {text117922} from 'react-text111';
 ddd
 const animatedComponents = makeAnimated();
 
